8d5f8a0dd67f939292df1aa7dde832f3c12b87eb
Kb_hacks <kamar254baraka@gmail.com>
2023-05-25 04:02:37 +0300

    added Manager.java
    Signed-off-by: Kb_hacks <kamar254baraka@gmail.com>

--- src/main/java/Manager.java
+++ src/main/java/Manager.java
@@ -1,2 +1,46 @@
-package PACKAGE_NAME;public class Manager {
+import com.kamarbaraka.practice.Employee;
+import java.time.LocalDate;
+
+/**
+ * this program demonstrates inheritance among classes\
+ * @author kamar baraka
+ * @version 1.0
+ * @since 25/5/2023*/
+
+public class Manager
+    extends com.kamarbaraka.practice.Employee {
+    private double bonus;
+    /**
+     *manager canonical constructor constructs a manager object
+     * and initializes the inherited employee fields
+     * @param name name of the manager
+     * @param salary the salary
+     * @param hireDay the date of hire*/
+    public Manager(String name, double salary, LocalDate hireDay){
+        super(name, salary, hireDay);
+        bonus = 0;
+    }
+    /**
+     * setBonus mutator method sets the bonus of the manager and adds it to the salary
+     * @param bonus the <em>double</em> bonus to be added*/
+    public void setBonus(double bonus){
+        this.bonus = bonus;
+        super.setSalary(this.bonus + super.getSalary());
+    }
+    /**
+     * override the super(Employee) class method to return the new salary(bonus included)
+     * @return double salary bonus included*/
+    @Override
+    public double getSalary(){
+        return super.getSalary() + bonus;
+    }
+    //unit test
+    public static void main(String[] args){
+        Employee baraka = new Employee("baraka", 4500, LocalDate.of(1999,8,22));
+        Manager boss = new Manager("boss", 50000, LocalDate.now());
+        boss.setBonus(250000);
+        for (Object each : boss.info())
+            System.out.println(each);
+
+    }
 }
